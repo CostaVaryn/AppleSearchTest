3daf0c0dc641dc0c3353243ae4f9c955bf57622d
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-08-02 11:41:40 +0300

    Update AppleTest.java

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -9,27 +9,7 @@ public class AppleTest extends BaseTest{
 
     @Test
     public void checkHref(){
-        // сокращение до двух строк, жестко
-        // Assert.assertTrue(new MainPage(BASE_URL).search(SEARCH_STRING).getHrefFromFirstArticle().contains("iphone-13"));
-
-        // MainPage mainPage = new MainPage(BASE_URL);
-
-        // mainPage.openWebSite(BASE_URL);
-
-        // было при обращении к классу
-        // mainPage.search(SEARCH_STRING);
-        // SearchPage searchPage = new SearchPage();
-        // String href = searchPage.getHrefFromFirstArticle();
-        // сократили и видим что осталось
-
-        //String href = mainPage.search(SEARCH_STRING).getHrefFromFirstArticle();
-
-        // переменная href будет содержать ссылку
-        // boolean contains = href.contains("iphone-13");
-        // проверка что ссылка содердит слово iphone-13
-
-        // Assert.assertTrue(new MainPage(BASE_URL).search(SEARCH_STRING).getHrefFromFirstArticle().contains("iphone-13"));
-        /** Assert.assertTrue(new MainPage(BASE_URL)
+                /** Assert.assertTrue(new MainPage(BASE_URL)
                 .search(SEARCH_STRING)
                 .getHrefFromFirstArticle()
                 .contains("iphone-13"));
