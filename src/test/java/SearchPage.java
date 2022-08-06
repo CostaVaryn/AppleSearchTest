0383c316007000641973c67bd6008fcd4f01dd87
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-08-06 12:57:54 +0300

    Update SearchPage.java

--- src/test/java/SearchPage.java
+++ src/test/java/SearchPage.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.ElementsCollection;
 
 import static com.codeborne.selenide.Selenide.$$x;
 
-
 public class SearchPage {
     private final ElementsCollection articleTitles = $$x("//h2//a");
 
